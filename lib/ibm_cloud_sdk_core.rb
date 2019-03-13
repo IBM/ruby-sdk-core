2d400c1f87bdcd0c8c92a565d1126bb9b161b264
Mamoon Raja <mamoonraja1@gmail.com>
2019-03-13 14:59:02 -0400

    feat(core): intialize the ibm cloud sdk core package

---
+++ lib/ibm_cloud_sdk_core.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Module for the IBM Cloud SDK Core
+module IBMCloudSdkCore
+  require_relative("./ibm_cloud_sdk_core/base_service.rb")
+end
---
+++ lib/ibm_cloud_sdk_core/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module IBMCloudSdkCore
+  VERSION = "0.0.0"
+end
